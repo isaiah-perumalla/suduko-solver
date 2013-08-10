3da68542a07458f3afe79fbf26caef40885629c6
isaiahperumalla <isaiahperumall@gmail.com>
2013-08-10 14:34:01 +0100

    added test for hard puzzles

--- test/SudukoSolverTests.java
+++ test/SudukoSolverTests.java
@@ -53,7 +53,7 @@ public class SudukoSolverTests {
     }
 
     @Test
-    public void testHardPuzzle() {
+    public void testHardPuzzle1() throws IOException {
 
         int[][] grid = new int[][]{
                 {8,5,0,0,0,2,4,0,0},
@@ -71,10 +71,30 @@ public class SudukoSolverTests {
 
         SudukoPuzzle sudukoPuzzle = new SudukoPuzzle(grid);
         Assert.assertTrue(sudukoPuzzle.solve());
-        try {
-            sudukoPuzzle.displayOn(System.out);
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+        sudukoPuzzle.displayOn(System.out);
+
+    }
+
+    @Test
+    public void testHardPuzzle2() throws IOException {
+
+        int[][] grid = new int[][]{
+                {0,0,5,3,0,0,0,0,0},
+                {8,0,0,0,0,0,0,2,0},
+                {0,7,0,0,1,0,5,0,0},
+                {4,0,0,0,0,5,3,0,0},
+                {0,1,0,0,7,0,0,0,6},
+                {0,0,3,2,0,0,0,8,0},
+                {0,6,0,5,0,0,0,0,9},
+                {0,0,4,0,0,0,0,3,0},
+                {0,0,0,0,0,9,7,0,0},
+
+        } ;
+
+
+        SudukoPuzzle sudukoPuzzle = new SudukoPuzzle(grid);
+        Assert.assertTrue(sudukoPuzzle.solve());
+        sudukoPuzzle.displayOn(System.out);
+
     }
 }
